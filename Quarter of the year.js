17f5834dbad356db5760d07ec1b8a1abe4991ff3
Natalia Gerasimovich <54632817+natasha17-spec@users.noreply.github.com>
2020-06-28 15:29:57 +0300

    Quarter of the year

---
+++ Quarter of the year.js
@@ -0,0 +1,10 @@
+const quarterOf = (month) => {
+    if (month>=0 && month<=3){
+        return 1
+    } else if (month>=3 && month<=6){
+        return 2
+    } else if (month>=6 && month<=9){
+        return 3
+    }else return 4
+}
+
